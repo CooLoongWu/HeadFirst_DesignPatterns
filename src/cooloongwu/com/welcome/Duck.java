152a3afa0dbc7d5ebfacdf786acdb8a1568d9a21
CooLoongWu <742420210@qq.com>
2018-01-16 14:35:39 +0800

    发现单纯的继承和接口都存在问题

--- src/cooloongwu/com/welcome/Duck.java
+++ src/cooloongwu/com/welcome/Duck.java
@@ -17,7 +17,14 @@ public class Duck {
         show("display");
     }
 
-    private void show(String str) {
+    /**
+     * 需要添加会飞的属性来将竞争者甩在后头。然而问题随之而来！！
+     */
+    void fly() {
+        show("fly");
+    }
+
+    void show(String str) {
         System.out.println(str);
     }
 }
--- src/cooloongwu/com/welcome/Main.java
+++ src/cooloongwu/com/welcome/Main.java
@@ -1,5 +1,8 @@
 package cooloongwu.com.welcome;
 
+/**
+ * 一个鸭子游戏
+ */
 public class Main {
 
     public static void main(String[] args) {
