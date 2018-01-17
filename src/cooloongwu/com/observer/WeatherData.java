b6607931d75b6beda8abbf2176ebff7f50cf142d
CooLoongWu <742420210@qq.com>
2018-01-17 13:32:31 +0800

    观察者模式-基础需求

---
+++ src/cooloongwu/com/observer/WeatherData.java
@@ -0,0 +1,46 @@
+package cooloongwu.com.observer;
+
+/**
+ * 天气测量数据类，返回包含温度，湿度，气压等数据
+ * <p>
+ * <p>
+ * 我们需要实现三个使用天气数据的布告板
+ * “目前状况”不高，“气象统计”不高，“天气预报”布告，一但有新的测量这些不高必须马上更新
+ * <p>
+ * 此系统必修可扩展，用户可以随想所欲的添加或者删除布告
+ * <p>
+ * <p>
+ * ~~错误的想法：
+ * 你可能首先想到最简单的方法就是创建三个布告板类，然后每个类中都有一个update()方法来处理更新
+ * 然后在 measurementsChanged() 方法中去调用布告类对象的 update(params... par) 方法
+ * 然而这样针对具体实现编程会导致我们后边在添加或者删除布告板类的时候必须要修改代码
+ * <p>
+ * 而且update(params... par)看起来是一个统一的接口，都有共同的参数温度，湿度，气压
+ */
+public class WeatherData {
+
+    void getTemperature() {
+
+    }
+
+    void getHumidity() {
+
+    }
+
+    void getPressure() {
+
+    }
+
+    /**
+     * 一但气象测量更新，此方法会被调用（我们不在乎此方法是如何被调用的，我们只在乎他被调用了）
+     */
+    void measurementsChanged() {
+
+    }
+
+
+    void mentsChanged() {
+
+    }
+
+}
