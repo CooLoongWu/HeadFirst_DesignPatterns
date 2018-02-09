e5b342c55995249ed2dfa26b45f18afb9a9594b6
CooLoongWu <742420210@qq.com>
2018-02-09 13:50:39 +0800

    复合模式-添加工厂模式

--- src/cooloongwu/com/compound/DuckSimulator.java
+++ src/cooloongwu/com/compound/DuckSimulator.java
@@ -4,7 +4,26 @@ public class DuckSimulator {
 
     public static void main(String[] args) {
         DuckSimulator simulator = new DuckSimulator();
-        simulator.simulate();
+
+        AbstractDuckFactory duckFactory = new CountingDuckFactory();
+        simulator.simulate(duckFactory);
+    }
+
+    private void simulate(AbstractDuckFactory duckFactory) {
+        Quackable mallardDuck = duckFactory.createMallardDuck();
+        Quackable redheadDuck = duckFactory.createRedheadDuck();
+        Quackable duckCall = duckFactory.createDuckCall();
+        Quackable rubberDuck = duckFactory.createRubberDuck();
+
+        Quackable gooseDuck = new GooseAdapter(new Goose());
+
+        System.out.println("Duck Simulator : With Abstract Factory");
+        simulate(mallardDuck);
+        simulate(redheadDuck);
+        simulate(duckCall);
+        simulate(rubberDuck);
+        simulate(gooseDuck);
+        System.out.println("\nThe ducks quacked " + QuackCounter.getNumberOfQuacks() + " times");
     }
 
     private void simulate() {
